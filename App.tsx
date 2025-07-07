33e792f3e193ef6b42ddf83d050b45b626698b81
Rangga M.P <sloth.7136@gmail.com>
2025-07-07 09:55:07 +0700

    test-3

--- App.tsx
+++ App.tsx
@@ -1,23 +1,37 @@
-    import React from 'react';
-    import { NavigationContainer } from '@react-navigation/native';
-    import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// TAMBAHKAN BARIS INI DI PALING ATAS untuk membungkam peringatan deprecation
+(global as any).RNFB_SILENCE_MODULAR_DEPRECATION_WARNINGS = true;
 
-    import OnboardingScreen from './OnboardingScreen';
-    import VerificationScreen from './VerificationScreen';
-    import OtpScreen from './OtpScreen';
-    import { RootStackParamList } from './types'; 
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-    const Stack = createNativeStackNavigator<RootStackParamList>();
+import OnboardingScreen from './OnboardingScreen';
+import VerificationScreen from './VerificationScreen';
+import OtpScreen from './OtpScreen';
+import { RootStackParamList } from './types'; 
 
-    export default function App() {
-      return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Onboarding">
-            <Stack.Screen name="Onboarding" component={OnboardingScreen} options={{ headerShown: false }} />
-            <Stack.Screen name="Verification" component={VerificationScreen} options={{ title: '' }} />
-            <Stack.Screen name="Otp" component={OtpScreen} options={{ title: 'Enter Verification Code' }} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      );
-    }
-    
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding">
+        <Stack.Screen 
+          name="Onboarding" 
+          component={OnboardingScreen} 
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen 
+          name="Verification" 
+          component={VerificationScreen} 
+          options={{ title: '' }}
+        />
+        <Stack.Screen 
+          name="Otp"
+          component={OtpScreen} 
+          options={{ title: 'Enter Verification Code' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
